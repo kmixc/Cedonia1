d903f4f72e74cba0e8251305734c676371cda593
Oliver <oliverkmiec24@gmail.com>
2022-12-10 15:23:05 -0500

    Home updates & Contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,19 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Modal from 'react-modal';
+import { AiOutlineClose } from 'react-icons/ai'
+import { Link } from "react-router-dom";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 export const ContactForm = () => {
     const form = useRef();
@@ -10,22 +24,94 @@ export const ContactForm = () => {
         emailjs.sendForm('service_qpoki0m', 'template_tqep1yw', form.current, 'Ek8m9YMYYi0hXmU-p')
             .then((result) => {
                 console.log(result.text);
+                openModal();
             }, (error) => {
                 console.log(error.text);
+                errorOpenModal();
             });
     };
 
+    Modal.setAppElement('#root');
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    const [errorModalIsOpen, errorSetIsOpen] = React.useState(false);
+
+    function errorOpenModal() {
+        errorSetIsOpen(true);
+    }
+
+    function errorCloseModal() {
+        errorSetIsOpen(false);
+    }
+
     return (
-        <div className='p-4 lg:flex justify-center'>
-            <form className='py-24 flex flex-col sm:w-full lg:w-2/4' ref={form} onSubmit={sendEmail}>
+        <div className='bg-zinc-200 lg:flex justify-center'>
+            <form className='py-4 flex flex-col sm:w-full lg:w-2/4' ref={form} onSubmit={sendEmail}>
                 <label className='text-zinc-900'>Name</label>
-                <input placeholder='John Smith' className='mt-1 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' type="text" required name="from_name" />
+                <input placeholder='John Smith' className='mt-1 text-zinc-900 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' type="text" required name="from_name" />
                 <label className='text-zinc-900'>Email</label>
-                <input placeholder='johnsmith@email.com' className='mt-1 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' type="email" required name="email_id" />
+                <input placeholder='johnsmith@email.com' className='mt-1 text-zinc-900 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' type="email" required name="email_id" />
                 <label className='text-zinc-900'>Message</label>
-                <textarea placeholder='Type your message to Cedonia here!' rows="5" className='mt-1 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' name="message" required />
-                <input className='text-white my-3 p-2 text-xl bg-red-500 rounded hover:bg-zinc-700 transition duration-150' type="submit" value="Send" />
+                <textarea placeholder='Type your message to Cedonia here!' rows="5" className='mt-1 text-zinc-900 p-2 block w-full rounded-md border-zinc-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-lg' name="message" required />
+                <input className='cursor-pointer text-white my-3 p-2 text-xl bg-red-500 rounded hover:bg-zinc-700 transition duration-150' type="submit" value="Send" />
             </form>
-        </div>
+
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <div className='flex flex-col'>
+                    <h2 className='text-red-500 text-xl' ref={(_subtitle) => (subtitle = _subtitle)}>Email Sent</h2>
+                    <button className='absolute top-0 right-0 p-2' onClick={closeModal}><AiOutlineClose className='text-zinc-900' size={"2em"} /></button>
+                    <div className='p-20'>
+                        <h2 className='text-zinc-900 text-xl'>Cedonia will send you an email back in the next few business days.</h2>
+                    </div>
+                    <div className='flex justify-around'>
+                        <Link className='transition hover:text-red-500' to="/">Home</Link>
+                        <Link className='transition hover:text-red-500' to="/about">About</Link>
+                        <Link className='transition hover:text-red-500' to="/quality">Quality</Link>
+                        <Link className='transition hover:text-red-500' to="/services">Services</Link>
+                    </div>
+                </div>
+            </Modal>
+            <Modal
+                isOpen={errorModalIsOpen}
+                onRequestClose={errorCloseModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <div className='flex flex-col'>
+                    <h2 className='text-red-500 text-xl' ref={(_subtitle) => (subtitle = _subtitle)}>Email Was Not Sent</h2>
+                    <button className='absolute top-0 right-0 p-2' onClick={closeModal}><AiOutlineClose className='text-zinc-900' size={"2em"} /></button>
+                    <div className='p-20'>
+                        <h2 className='text-zinc-900 text-xl'>Please try again or feel free to give us a phone call.</h2>
+                    </div>
+                    <div className='flex justify-around'>
+                        <Link className='transition hover:text-red-500' to="/">Home</Link>
+                        <Link className='transition hover:text-red-500' to="/about">About</Link>
+                        <Link className='transition hover:text-red-500' to="/quality">Quality</Link>
+                        <Link className='transition hover:text-red-500' to="/services">Services</Link>
+                    </div>
+                </div>
+            </Modal>
+        </div >
     );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ContactForm } from '../components/ContactForm';
-
+import { AiOutlineMail, AiOutlineHome, AiOutlinePhone } from 'react-icons/ai'
 
 export default function Contact() {
     return (
@@ -16,16 +16,46 @@ export default function Contact() {
                         </h2>
                     </div>
                 </div>
-                <iframe width="100%" height="551" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=967&amp;height=551&amp;hl=en&amp;q=2405%20Lucknow%20Dr%20Mississauga+(Cedonia)&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href='https://maps-generator.com/'></a>
-                <div className='bg-zinc-200'>
+                <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=967&amp;height=551&amp;hl=en&amp;q=2405%20Lucknow%20Dr%20Mississauga+(Cedonia)&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href='https://maps-generator.com/'></a>
+                <div className='bg-zinc-200 p-4'>
+                    <div class="text-center w-full mx-auto pt-12 px-4 sm:px-6 lg:pt-8 lg:px-8 z-20">
+                        <h2 class="text-3xl flex flex-wrap justify-center font-extrabold text-black sm:text-4xl">
+                            <span class="block">
+                                Email Us Now
+                            </span>
+                        </h2>
+                    </div>
                     <ContactForm></ContactForm>
                 </div>
 
                 <div className='bg-zinc-300 text-zinc-900'>
-                    <div className='flex justify-center py-16'>
-                        <h3 className='px-4'>meet us</h3>
-                        <h3 className='px-4'>call us</h3>
-                        <h3 className='px-4'>mail us</h3>
+                    <div className='flex flex-col lg:flex-row text-sm lg:text-lg justify-center py-16'>
+                        <div className='flex px-4 items-center justify-center'>
+                            <AiOutlineMail size={"2em"} />
+                            <p className='m-1' target="social" href="#">
+                                Meet Us:
+                                2405 Lucknow Drive,
+                                Mississauga, On, L5S 1H9
+                            </p>
+                        </div>
+                        <div className='flex px-4 items-center justify-center'>
+                            <AiOutlineHome size={"2em"} />
+                            <p className='m-1' target="social" href="#">
+                                Call Us:
+                            </p>
+                            <a href="tel:905-564-2033" className='text-red-500 underline hover:text-red-700'>
+                                905-564-2033
+                            </a>
+                        </div>
+                        <div className='flex px-4 items-center justify-center'>
+                            <AiOutlinePhone size={"2em"} />
+                            <p className='m-1' target="social" href="#">
+                                Mail Us:
+                            </p>
+                            <a href="mailto:" className='text-red-500 underline '>
+                                inquiries@cedonia.ca
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
